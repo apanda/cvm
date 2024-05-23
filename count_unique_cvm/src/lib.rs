0b6cfcd9e1be181ee434df949c9404bf065e1a41
Aurojit Panda <apanda@cs.nyu.edu>
2024-05-23 15:12:55 -0400

    Document the CVM package

--- count_unique_cvm/src/lib.rs
+++ count_unique_cvm/src/lib.rs
@@ -1,7 +1,33 @@
+//! An implementation of the CVM algorithm
+//! The [CVM]((https://cs.stanford.edu/~knuth/papers/cvm-note.pdf) algorithm provides
+//! a probabilistic data structure for counting the number of unique elements in a stream.
+//! This is an implementation of the simple version of the algorithm.
+//!
+//! # Example
+//! ```
+//! use count_unique_cvm::*;
+//! use rand::prelude::*;
+//!
+//! // Create a CountUnique CVM which stores at most 4 elements.
+//! let mut c = CountUnique::new(rand::thread_rng(), 4);
+//! let v = vec![0..100];
+//! for i in v {
+//!     c.add_token(*v);
+//! }
+//! // The actual value depends on the random number generator,
+//! // but should be close.
+//! println!("Estimated number of tokens are {}", c.estimate());
+//! ```
+#![deny(missing_docs)]
+
 use conv::*;
 use rand::Rng;
 use treap::{Element, Treap};
 use treap_non_random as treap;
+
+/// The CVM algorithm state. `T` is the type of tokens that are
+/// being counted, and `R` is the random number generator that should
+/// be used.
 pub struct CountUnique<T: Ord + Clone, R: Rng> {
     treap: Treap<T, f32>,
     rng: R,
@@ -14,6 +40,11 @@ where
     T: Ord + Clone,
     R: Rng,
 {
+    /// Create a new `CountUnique` structure. `sz` is the number
+    /// of elements stored by CVM, and must be non-zero.
+    ///
+    /// # Panics
+    /// Function will panic if called with `sz < 1`.
     pub fn new(r: R, sz: usize) -> Self {
         if sz < 1 {
             panic!("Cannot count without state");
@@ -26,6 +57,7 @@ where
         }
     }
 
+    /// Add a token to the CVM.
     pub fn add_token(&mut self, t: T) {
         let u = self.rng.gen::<f32>();
         self.treap.delete(&t);
@@ -49,12 +81,17 @@ where
         }
     }
 
+    /// Return the current estimated number of tokens. Note that
+    /// CVM is a probabilistic algorithm and the returned value can
+    /// be far from the actual value, the algorithm merely guarantees
+    /// that it will return the correct value in expectation.
     pub fn estimate(&self) -> Result<f64, PosOverflow<usize>> {
         let f = f64::value_from(self.treap.size())?;
         let p = self.p as f64;
         Ok(f / p)
     }
 
+    /// Reset the CVM state allowing the structure to be reused.
     pub fn reset(&mut self) {
         self.treap.reset();
         self.p = 1.0f32;
